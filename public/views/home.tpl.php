d05b31221f38a1b700a3e0f62dd64447079b10c9
Juan Torres <juanin.torres@gmail.com>
2015-06-18 06:07:02 -0300

    Agregada la cita inicial

--- public/views/home.tpl.php
+++ public/views/home.tpl.php
@@ -1,4 +1,6 @@
-
-<h4><?= $titulo ?></h4>
-
-<p><a href="contactos">Contactanos</a></p>
+<div class="jumbotron">
+<blockquote>
+    <p>Si la gente no piensa que las matemáticas son simples, es sólo porque no se dan cuenta de lo complicada que es la vida.</p>
+    <footer>John Von Neumann</footer>
+</blockquote>
+</div>
