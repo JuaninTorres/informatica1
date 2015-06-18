2805ebc59358c635487934f41f923436696b1851
Juan Torres <juanin.torres@gmail.com>
2015-06-18 04:55:46 -0300

    Primera version con el mini framework
    Aún falta crear todos los controladores y sus vistas

---
+++ public/helpers.php
@@ -0,0 +1,36 @@
+<?php
+
+// Declarando una funcion
+function view($template, $vars = array())
+{
+    extract($vars);
+
+    require "views/$template.tpl.php";
+}
+
+function controller($name)
+{
+    if (empty($name))
+    {
+        $name = 'home';
+    }
+
+    $file = "controllers/$name.php";
+
+    if (file_exists($file))
+    {
+        require $file;
+    }
+    else
+    {
+        header("HTTP/1.0 404 Not Found");
+        exit("Pagina no encontrada");
+    }
+}
+
+function dd($vars)
+{
+    var_dump($vars);
+
+    die(1);
+}
