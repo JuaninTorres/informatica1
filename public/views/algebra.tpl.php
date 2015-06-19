77181285b491df5f00c6385f06593f6645dbf058
Juan Torres <juanin.torres@gmail.com>
2015-06-19 12:50:59 -0300

    Faltante

---
+++ public/views/algebra.tpl.php
@@ -0,0 +1,7 @@
+<div class="page-header">
+    <h1>Algebra</h1>
+</div>
+<ul>
+    <li><a href="/algebra/productos-notables-y-criterios-de-factorizacion">Productos notables y criterios de factorización</a></li>
+    <li><a href="/algebra/ecuaciones-de-primer-grado">Ecuaciones de primer grado</a></li>
+</ul>
