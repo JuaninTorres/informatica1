68a39c5aabc5303855763cf8877b8ad18decd8c6
nicolasgonzalezm <nicolas.gonzalez.martinez@gmail.com>
2015-06-22 19:30:50 -0300

    Agregue Apuntes de logaritmos

--- public/views/numeros.logaritmos.tpl.php
+++ public/views/numeros.logaritmos.tpl.php
@@ -1,3 +1,47 @@
-<div class="page-header">
-    <h1>Logaritmos</h1>
+<div class="jumbotron">
+    <div class="container">
+        <h1>Logaritmos</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12">
+        <h2>Definiciones</h2>
+        <p>Aquí debo colocar un texto interesante</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Apuntes</h3>
+            </div>
+            <div class="panel-body">
+                <ul>
+                    <li>
+                        <a href="/textos/numeros/1_definicion_de_conjunto/definicion_de_conjuntos_ver1.pdf" download="download"/>
+                            Apunte 1
+                        </a>
+                    </li>
+                    <li>
+                        <a href="/textos/numeros/1_definicion_de_conjunto/definicion_de_conjuntos_ver1.pdf" download="download"/>
+                            Apunte 1
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">Guías</h3>
+            </div>
+            <div class="panel-body">
+
+            </div>
+        </div>
+    </div>
+
 </div>
