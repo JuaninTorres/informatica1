62327a02555ff00bfd9615788cb3af4c67197f8d
nicolasgonzalezm <nicolas.gonzalez.martinez@gmail.com>
2015-06-24 14:09:33 -0300

    actualizacion de funciones

--- public/views/funciones.funcion_logaritmo.tpl.php
+++ public/views/funciones.funcion_logaritmo.tpl.php
@@ -1,3 +1,41 @@
-<div class="page-header">
-    <h1>Función Logaritmo</h1>
+<div class="jumbotron">
+    <div class="container">
+        <h1>Función Logaritmo</h1>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12">
+        <h2>Definiciones</h2>
+        <p>Aquí debo colocar un texto interesante</p>
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-6">
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Apuntes</h3>
+            </div>
+            <div class="panel-body">
+                <ul> 
+                    <li> 
+                        <a href="/textos/funciones/funcion_logaritmo/guia_de_funciones.pdf" download/>
+                            Apuntes 
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="col-sm-6">
+        <div class="panel panel-info">
+            <div class="panel-heading">
+                <h3 class="panel-title">Guías</h3>
+            </div>
+            <div class="panel-body">
+               
+            </div>
+        </div>
+    </div>
 </div>
