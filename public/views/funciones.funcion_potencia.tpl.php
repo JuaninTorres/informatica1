4f59eac5be563725fb0ad19bf73026e36d6285dd
nicolasgonzalezm <nicolas.gonzalez.martinez@gmail.com>
2015-07-03 22:43:24 -0300

    Potencias
    cambio potencias

--- public/views/funciones.funcion_potencia.tpl.php
+++ public/views/funciones.funcion_potencia.tpl.php
@@ -23,9 +23,15 @@
                 <h3 class="panel-title"><span class="glyphicon glyphicon-book" aria-hidden="true"></span> Apuntes</h3>
             </div>
             <div class="list-group">
-                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/guia_de_funciones.pdf" download>
-                    Apuntes
-                </a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/apuntes_trigonm.pdf" download>Apunte 1</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/funcion.pdf" download>Apunte 2</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/guia_de_trigonometria.docx" download>Apunte 3</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/problemas_de_aplicacion_trigonometria.pdf" download>Apunte 4</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_1.pdf" download>Apunte 5</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_2.pdf" download>Apunte 6</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_3.pdf" download>Apunte 7</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_4.pdf" download>Apunte 8</a>
+
             </div>
         </div>
     </div>
@@ -35,6 +41,14 @@
                 <h3 class="panel-title"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Guías</h3>
             </div>
             <div class="list-group">
+            <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/apuntes_trigonm.pdf" download>Guía 1</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/funcion.pdf" download>Guía 2</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/guia_de_trigonometria.docx" download>Guía 3</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/problemas_de_aplicacion_trigonometria.pdf" download>Guía 4</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_1.pdf" download>Guía 5</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_2.pdf" download>Guía 6</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_3.pdf" download>Guía 7</a>
+                <a class="list-group-item" href="/textos/funciones/funcion_logaritmo/taller_4.pdf" download>Guía 8</a>
             </div>
         </div>
     </div>
