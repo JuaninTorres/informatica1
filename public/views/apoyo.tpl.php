5e72ce1be76c26ecafa5e73345aa178fc04455fa
nicolasgonzalezm <nicolas.gonzalez.martinez@gmail.com>
2015-07-06 23:00:28 -0300

    titulo
    poner titulo a secciones

--- public/views/apoyo.tpl.php
+++ public/views/apoyo.tpl.php
@@ -13,7 +13,9 @@
     <div class="col-md-3">
         <div class="content">
             <h3>Números</h3>
+             <h4>Logaritmos</h4>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/3daASOhcRRQ" frameborder="0" allowfullscreen></iframe>
+            <h4>Operatoria en Q</h4>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/kxDeTxbnk-c" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
