44768f6fbdc73d80aa150cdba7cab52b0aca50cb
nicolasgonzalezm <nicolas.gonzalez.martinez@gmail.com>
2015-07-08 23:37:30 -0300

    videos de apoyo
    crear secciones de videos

--- public/views/apoyo.tpl.php
+++ public/views/apoyo.tpl.php
@@ -13,30 +13,42 @@
     <div class="col-md-3">
         <div class="content">
             <h3>Números</h3>
-             <h4>Logaritmos</h4>
+            <h4>Potencias</h4>
+            <h4>Raices</h4>
+            <h4>Logaritmos</h4>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/3daASOhcRRQ" frameborder="0" allowfullscreen></iframe>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/jl8xT_QtDJE" frameborder="0" allowfullscreen></iframe>
-           
-           <h4>Operatoria en Q</h4>
+            <h4>Operatoria en Q</h4>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/kxDeTxbnk-c" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
     <div class="col-md-3">
         <div class="content">
             <h3>Álgebra</h3>
+             <h4>Ecuaciones de Primer Grado</h4>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/h4gt8tsVJbw" frameborder="0" allowfullscreen></iframe>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/BdRAhV0JDjM" frameborder="0" allowfullscreen></iframe>
+             <h4>Productos Notables</h4>
+            <h4>Factorización</h4>
         </div>
     </div>
     <div class="col-md-3">
         <div class="content">
             <h3>Funciones</h3>
+             <h4>Función Cuadrática</h4>
+              <h4>Función Logaritmo</h4>
+               <h4>Función Potencia</h4>
+                <h4>Función Raíz</h4>
             <iframe width="280" height="158" src="https://www.youtube.com/embed/E6ysFJEIyEc" frameborder="0" allowfullscreen></iframe>
         </div>
     </div>
     <div class="col-md-3">
         <div class="content">
             <h3>Geometría</h3>
+             <h4>Teorema de Pitagoras</h4>
+              <h4>Teorema de Thales</h4>
+               <h4>Teorema de Euclides</h4>
+                <h4>Geometria Analitica</h4>
         </div>
     </div>
 </div>
